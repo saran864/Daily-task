f26fb6696211fc99e2578a5f62d4a67868638719
saran864 <112957453+saran864@users.noreply.github.com>
2022-09-22 16:39:02 +0530

    Update GradingStudents.java

--- GradingStudents.java
+++ GradingStudents.java
@@ -60,32 +60,34 @@ import java.util.regex.*;
 
 public class Solution {
 
-    static int[] solve(int[] grades){
-        for (int i = 0; i < grades.length; i++) {
-            if (grades[i] >= 38) {
-                int nextMultipleOfFive = 5 - (grades[i] % 5) + grades[i];
-                if (nextMultipleOfFive - grades[i] < 3) {
-                    grades[i] = nextMultipleOfFive;
-                }
-            }
-        }
-
-        return grades;
-    }
+  
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int[] grades = new int[n];
-        for(int grades_i=0; grades_i < n; grades_i++){
-            grades[grades_i] = in.nextInt();
+        Scanner s = new Scanner(System.in);
+        int n=s.nextInt();
+        int remainder,multiply;
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=s.nextInt();
         }
-        int[] result = solve(grades);
-        for (int i = 0; i < result.length; i++) {
-            System.out.print(result[i] + (i != result.length - 1 ? "\n" : ""));
-        }
-        System.out.println("");
+        for(int i=0;i<n;i++)
+        {
+           if(arr[i]>=38)
+           {
+               multiply=5-(arr[i]%5)+arr[i];
+              if(multiply-arr[i]<3)
+              {
+                  arr[i]=multiply;
+                  System.out.println(arr[i]);
+              }
+              else
+              System.out.println(arr[i]);
+           }
+           else 
+           System.out.println(arr[i]);
 
 
     }
+}
 }
