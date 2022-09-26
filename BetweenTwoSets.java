30f5dcaf33e354d57975406222e71181211cb1e4
saran864 <112957453+saran864@users.noreply.github.com>
2022-09-26 15:30:03 +0530

    Create BetweenTwoSets.java

---
+++ BetweenTwoSets.java
@@ -0,0 +1,130 @@
+/*
+There will be two arrays of integers. Determine all integers that satisfy the following two conditions:
+
+The elements of the first array are all factors of the integer being considered
+The integer being considered is a factor of all elements of the second array
+These numbers are referred to as being between the two arrays. Determine how many such numbers exist.
+
+Example
+
+
+There are two numbers between the arrays:  and .
+, ,  and  for the first value.
+,  and ,  for the second value. Return .
+
+Function Description
+
+Complete the getTotalX function in the editor below. It should return the number of integers that are betwen the sets.
+
+getTotalX has the following parameter(s):
+
+int a[n]: an array of integers
+int b[m]: an array of integers
+Returns
+
+int: the number of integers that are between the sets
+Input Format
+
+The first line contains two space-separated integers,  and , the number of elements in arrays  and .
+The second line contains  distinct space-separated integers  where .
+The third line contains  distinct space-separated integers  where .
+
+Constraints
+
+Sample Input
+
+2 3
+2 4
+16 32 96
+Sample Output
+
+3
+Explanation
+
+2 and 4 divide evenly into 4, 8, 12 and 16.
+4, 8 and 16 divide evenly into 16, 32, 96.
+
+4, 8 and 16 are the only three numbers for which each element of a is a factor and each is a factor of all elements of b.
+*/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'getTotalX' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
+
+    public static int getTotalX(List<Integer> a, List<Integer> b) {
+    int count = 0, current_factor = a.get(a.size() - 1), flag;
+        while (current_factor <= b.get(0)) {
+            flag = 0;
+            for (int i = 0; i < a.size(); i++) {
+                if (current_factor % a.get(i) != 0) {
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0) {
+                for (int i = 0; i < b.size(); i++) {
+                    if (b.get(i) % current_factor != 0) {
+                        flag = 1;
+                        break;
+                    }
+                }
+
+            }
+            if (flag == 0) {
+                count++;
+            }
+            current_factor++;
+        }
+        return count;
+    }
+
+    
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integrer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> brr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int total = Result.getTotalX(arr, brr);
+
+        bufferedWriter.write(String.valueOf(total));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
