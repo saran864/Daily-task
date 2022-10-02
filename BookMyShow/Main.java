242f0cad9237efce1f3391e30b7f2559b499f87a
saran864 <112957453+saran864@users.noreply.github.com>
2022-10-02 21:17:16 +0530

    Create Main.java

---
+++ BookMyShow/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+ import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+	
+	    
+		public static void main(String[] args) {
+		    Boomyshow op =new Boomyshow();
+		    op.function();
+		    
+		    
+		   
+		
+	}
+}
