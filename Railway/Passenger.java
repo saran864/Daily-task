76bb752600b88829ff9ad4bcb66917ed01c870cb
saran864 <112957453+saran864@users.noreply.github.com>
2022-10-06 18:43:59 +0530

    Create Passenger.java

---
+++ Railway/Passenger.java
@@ -0,0 +1,49 @@
+class Passenger {
+    private String mailid;
+	private String  name;
+	private byte age;
+	private String  password;
+	
+
+	 
+	Passenger(String mailid,String name,byte age,String password){
+		this.name=name;
+		this.mailid=mailid;
+		this.age=age;
+		this.password=password;
+		
+	}
+		Passenger(){}
+	
+	public String getMailid() { 
+		return mailid;
+	}
+
+	public void setMailid(String mailid) {
+		this.mailid = mailid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public byte getAge() {
+		return age;
+	}
+
+	public void setAge(byte age) {
+		this.age = age;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
