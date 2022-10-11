4e5c975c301701db3ce540bc55bf425ef769d492
saran864 <112957453+saran864@users.noreply.github.com>
2022-10-11 10:38:14 +0530

    Update Jdbc.java

--- Railway/Jdbc.java
+++ Railway/Jdbc.java
@@ -1,12 +1,12 @@
 package TrainReserve;
-import java.sql.*;
+import java.sql.*;//step :1 
 public class Jdbc {
 
 
     public static Connection jdbcConnection(){
         try
         {
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/railwayResevation","root","password");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/TrainTicketBooking","root","password");//step 2
          
         }catch (Exception e){
             e.printStackTrace();
