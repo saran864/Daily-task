922c39b55dc597921be1d65dbd62c48d63b93d9c
saran864 <112957453+saran864@users.noreply.github.com>
2022-10-11 10:41:36 +0530

    Update User.java

--- Railway/User.java
+++ Railway/User.java
@@ -1,74 +1,73 @@
 package TrainReserve;
 
 import java.util.*;
-
-class User
+public class User 
 {
-    Admin ad=new Admin();
-    
-   ArrayList <Passenger> user=new ArrayList <> ();
-   
-   User()
-   {
-	 user.add( new Passenger ("saran@gmail.com","saran",(byte)21,"sara123"));
-	 user.add(new Passenger ("suren@gmail.com","suren",(byte)22,"suren@123"));
-	 user.add(new Passenger ("manoaran@gmail.com","mano",(byte)22,"mano@123"));
- 	 user.add( new Passenger ("raj@gmail.com","raj",(byte)22,"raj@1213"));
-   }
-   
-    public void userList(){
-    
- 	 for(Passenger pk : user )
-          {
-              System.out.println(pk.getName());
-          }
-    }
-    Scanner sc=new Scanner(System.in);
-   
-  
-    public void signUp(){
-           System.out.println("Enter Name :");
-          String name=sc.next();
-          System.out.println("Enter Age :");
-          byte age=sc.nextByte();
-          System.out.println("Enter Email :");
-          String mail=sc.next();
-           System.out.println("Enter Password :");
-          String pass=sc.next();
-           user.add(new Passenger(mail,name,age,pass));
-         System.out.println("Data added Successfully!");
-         System.out.println();
-          System.out.println("Welcome!"+name);
-          ad.ticket();
-          
-         
-        
-        
-      }
-    public void logIn(){
-        boolean check =false;
-        String loginname="";
-     System.out.println("Enter Email :");
-          String mail=sc.next();
-           System.out.println("Enter Password :");
-         String pass=sc.next();
-         for(Passenger pk : user )
-         {
-         if (pk.getMailid().equals(mail)&&pk.getPassword().equals(pass))
-         {
-          loginname=pk.getName();
-             check=true;
-         }
-                
-         }
-         if(check==true)
-         {
-          System.out.println("Welcome "+loginname);
-                  ad.ticket();
-         }
-    
-         else
-         System.out.println("Username or password wrong!");
-    }
+	 Admin admin=new Admin();
+	    
+	   ArrayList <Passenger> userList=new ArrayList <> ();
+	   
+	   User()
+	   {
+		 userList.add( new Passenger ("saran@gmail.com","Saran",(byte)21,"sara123"));
+		 userList.add(new Passenger ("suren@gmail.com","Suren",(byte)22,"sur@123"));
+		 userList.add(new Passenger ("mani@gmail.com","Mani",(byte)22,"mani*12"));
+	 	 userList.add( new Passenger ("raj@gmail.com","Raj",(byte)22,"raj#13"));
+	   }
+	   
+	    public void userList(){
+	    
+	 	 for(Passenger passenger : userList )
+	          {
+	              System.out.println("Passenger Name: "+passenger.getName()+"Ps");
+	          }
+	    }
+	    Scanner userInput=new Scanner(System.in);
+	   
+	  
+	    public void signUp(){
+	           System.out.println("Enter Name :");
+	          String name=userInput.next();
+	          System.out.println("Enter Age :");
+	          byte age=userInput.nextByte();
+	          System.out.println("Enter Email :");
+	          String mail=userInput.next();
+	           System.out.println("Enter Password :");
+	          String pass=userInput.next();
+	           userList.add(new Passenger(mail,name,age,pass));
+	         System.out.println("Data added Successfully!");
+	         System.out.println();
+	          System.out.println("Welcome!"+name);
+	          admin.ticket();
+	          
+	         
+	        
+	        
+	      }
+	    public void logIn(){
+	        boolean checkIf =false;
+	        String loginname="";
+	     System.out.println("Enter Email :");
+	          String mail=userInput.next();
+	           System.out.println("Enter Password :");
+	         String pass=userInput.next();
+	         for(Passenger passenger : userList )
+	         {
+	         if (passenger.getMailid().equals(mail)&&passenger.getPassword().equals(pass))
+	         {
+	          loginname=passenger.getName();
+	          checkIf=true;
+	         }
+	                
+	         }
+	         if(checkIf==true)
+	         {
+	          System.out.println("Welcome "+loginname);
+	          admin.ticket();
+	         }
+	    
+	         else
+	         System.out.println("Username or password wrong!");
+	    }
 
-}	
+}
