8d9293e4898d8cfae0ad8b6dd4e985708ba3e371
saran864 <112957453+saran864@users.noreply.github.com>
2022-10-11 10:39:31 +0530

    Update and rename Main.java to TrainMain.java

---
+++ Railway/TrainMain.java
@@ -0,0 +1,12 @@
+package TrainReserve;
+
+public class TrainMain {
+
+	public static void main(String[] args) {
+System.out.println("Welcome to Southern Railway Reservation System");
+		Operation operation = new Operation();
+		operation.function();
+
+	}
+
+}
