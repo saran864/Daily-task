3606265fa9265b9fc3da91234cadb737a83e5877
saran864 <112957453+saran864@users.noreply.github.com>
2022-10-12 17:43:32 +0530

    Update Operation.java

--- Railway/Operation.java
+++ Railway/Operation.java
@@ -4,12 +4,10 @@ package TrainReserve;
 import java.util.Scanner;
 
 public class Operation {
-	User user = new User();
 	Admin admin = new Admin();
-	private String adminid = "admin001";
-	private String password = "rrb@123";
+	User user=new User();
 	Scanner getInput = new Scanner(System.in);
-	public void function() {
+	public  void function() {
 
 		boolean checkIf = true;
 		while (checkIf) {
@@ -49,47 +47,7 @@ public class Operation {
 				break;
 			}
 			case 3: {
-				boolean d;
-				System.out.println("->Enter Loginid :");
-				String adminmail = getInput.next();
-				System.out.println("->Enter password:");
-				String adminpass = getInput.next();
-				if (adminid.equals(adminmail) && adminpass.equals(password)) {
-					d = true;
-					while (d) {
-
-						System.out.println("Welcome Admin\n");
-						System.out.println("1.Add Trains\n2.View Trains\n3.View Customers\n4.Back");
-						int choice = getInput.nextInt();
-						// c=false;
-						switch (choice) {
-						case 1: {
-							admin.addTrain();
-							break;
-						}
-						case 2: {
-							admin.viewTrainList();
-							break;
-						}
-
-						case 3: {
-							user.userList();
-							break;
-						}
-
-						case 4: {
-							// c=true;
-							d = false;
-							System.out.println("1.User Signup\n2.User Login\n3.Admin Login\n4.Exit");
-							break;
-						}
-
-						}
-
-					}
-				} else
-					System.out.println("Worng Credentials...!");
-
+				admin.adminAccess();
 			}
 			case 4: {
 				checkIf = false;
