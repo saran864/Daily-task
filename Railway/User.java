6ef9e95ce563cb410699163d528a37d7838a3375
saran864 <112957453+saran864@users.noreply.github.com>
2022-10-12 17:45:09 +0530

    Update User.java

--- Railway/User.java
+++ Railway/User.java
@@ -1,10 +1,14 @@
 package TrainReserve;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.*;
 public class User 
 {
+	int train_no;
 	 Admin admin=new Admin();
-	    
+		Scanner getInput = new Scanner(System.in);
+
 	   ArrayList <Passenger> userList=new ArrayList <> ();
 	   
 	   User()
@@ -14,6 +18,41 @@ public class User
 		 userList.add(new Passenger ("mani@gmail.com","Mani",(byte)22,"mani*12"));
 	 	 userList.add( new Passenger ("raj@gmail.com","Raj",(byte)22,"raj#13"));
 	   }
+		boolean b = true;
+
+		public void userAccess() {
+			while (b) {
+			 
+				System.out.println("\n1.BookTicket\n2.SearchTrain\n3.Back");
+				int ipt = getInput.nextInt();
+
+				switch (ipt) {
+				case 1: {
+					ticketBooking();
+					break;
+				    }
+				case 2: {
+					searchTrain() ;
+				
+					break;
+
+				}
+
+				case 3: {
+					b = false;
+					break;
+				}
+
+				}
+
+			}
+
+		}
+	   
+	   
+	   
+	   
+	   
 	   
 	    public void userList(){
 	    
@@ -23,22 +62,21 @@ public class User
 	          }
 	    }
 	    Scanner userInput=new Scanner(System.in);
-	   
-	  
+	   Passenger passenger=new Passenger();	  
 	    public void signUp(){
-	           System.out.println("Enter Name :");
+	          System.out.println("Enter Name :");
 	          String name=userInput.next();
 	          System.out.println("Enter Age :");
 	          byte age=userInput.nextByte();
 	          System.out.println("Enter Email :");
 	          String mail=userInput.next();
-	           System.out.println("Enter Password :");
+	          System.out.println("Enter Password :");
 	          String pass=userInput.next();
-	           userList.add(new Passenger(mail,name,age,pass));
-	         System.out.println("Data added Successfully!");
-	         System.out.println();
+	          userList.add(new Passenger(mail,name,age,pass));
+	          System.out.println("Data added Successfully!");
+	          System.out.println();
 	          System.out.println("Welcome!"+name);
-	          admin.ticket();
+	          userAccess();
 	          
 	         
 	        
@@ -63,11 +101,108 @@ public class User
 	         if(checkIf==true)
 	         {
 	          System.out.println("Welcome "+loginname);
-	          admin.ticket();
+	            userAccess();
 	         }
 	    
 	         else
 	         System.out.println("Username or password wrong!");
 	    }
+	    
+	    
+	    
+	    private  void ticketBooking()
+	    {
+	    admin.viewTrainList();
+		System.out.println("Enter the Train No");
+		 train_no = getInput.nextInt();
+		System.out.println("Enter no.of tickets:");
+		int noOfTickets = getInput.nextInt();
+		try {
 
-}
+			PreparedStatement ps1 = Jdbc.jdbcConnection().prepareStatement(
+					"update train set ticket_count =ticket_count-" + noOfTickets + " where trainno=?");
+			ps1.setInt(1, train_no);
+			int queryExecution = ps1.executeUpdate();
+			//ps1.close();
+			PreparedStatement ps2 = Jdbc.jdbcConnection()
+					.prepareStatement("select ticket_cost,train_name from train where trainno=?");
+			ps2.setInt(1, train_no);
+			//ps2.close();
+			ResultSet rs = ps2.executeQuery();
+			rs.next();
+			
+			
+			int amt = rs.getInt(1);
+			String trainname = rs.getString(2);
+			int total_amount = amt * noOfTickets;
+			System.out.println("************************");
+			System.out.println("Train No = " + train_no + "\nTrain Name = " + trainname + "\nTotal Amount  = " + total_amount+"\nHappy journey With"+trainname);
+		  System.out.println("************************");
+		//  rs.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	    
+	    }
+	    private  void searchTrain()  
+	    {System.out.println("1.Chennai-Senkottai\n2.Madurai-Chennai\n3.Chennai-Thanjavur\n4.Tuticorin-Chennai \n5.Trichy -Chennai\n6.Nagercoil-Chennai");
+	    	int trainfromto = getInput.nextInt();
+			switch (trainfromto)
+			{
+			case 1:
+			{
+				
+			 train_no=122348;
+			 
+				break;
+			}
+			case 2:
+			{train_no=122349;
+				break;
+			}
+			case 3:
+			{train_no=122350;
+				break;
+			}
+			case 4:
+			{train_no=122351;
+				break;
+			}
+			case 5:
+			{train_no=122352;
+				break;
+			}
+			case 6:
+			{train_no=122353;
+				break;
+			}
+			}
+			try {
+			PreparedStatement ps3 = Jdbc.jdbcConnection()
+					.prepareStatement("select * from train where trainno=?");
+			ps3.setInt(1, train_no);
+			ResultSet rs = ps3.executeQuery();
+			rs.next();
+			System.out.println("--------Available Trains------- ");
+			System.out.println(
+					"TrainNo    TrainName                     From            To         Tick.Count Tick.Cost");
+			System.out.println(rs.getInt("trainno") + "\t" + rs.getString("train_name") +" \t"+ rs.getString("train_from") +"\t"+ rs.getString("train_to")
+			 +"\t"+ rs.getInt("ticket_count") +"\t"+ rs.getString("ticket_cost"));}
+			
+			 catch (Exception e) {
+					e.printStackTrace();
+				}
+	    	
+	    	
+	    	
+	    	
+	    }
+	    
+	    }
+	    
+	    
+	    
+
+
